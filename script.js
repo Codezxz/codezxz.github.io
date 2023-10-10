d1459c9610c90fde600608198c07fb5f02c13118
Nirnay N. Sheshware <107663236+Codezxz@users.noreply.github.com>
2023-10-10 17:52:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ inputField.addEventListener('keydown', function (e) {
         terminal.innerHTML += `<div>$ ${command}</div>`;
         
         // Send the command to a server for processing (replace with your server's URL)
-        fetch('https://www.google.com/search?q='+command, {
+        fetch('https://www.google.com/search?q=', {
             method: 'POST',
             body: JSON.stringify({ command }),
             headers: {
