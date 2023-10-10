4cd8f134a8172ffb372f70b09594f7b71d0bc705
Nirnay N. Sheshware <107663236+Codezxz@users.noreply.github.com>
2023-10-10 17:18:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+body {
+    font-family: monospace;
+    background-color: black;
+    color: white;
+    margin: 0;
+    padding: 0;
+}
+
+.terminal {
+    width: 80%;
+    max-width: 600px;
+    margin: 50px auto;
+    background-color: black;
+    border: 1px solid white;
+    padding: 10px;
+    box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
+}
+
+#input {
+    width: 100%;
+    border: none;
+    background: transparent;
+    color: white;
+    outline: none;
+}
