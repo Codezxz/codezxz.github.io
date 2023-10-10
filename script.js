9cae040dba0b49d67fc47a8c52df53f27b1579af
Nirnay N. Sheshware <107663236+Codezxz@users.noreply.github.com>
2023-10-10 17:23:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-body {
-    font-family: monospace;
-    background-color: black;
-    color: white;
-    margin: 0;
-    padding: 0;
-}
+const terminal = document.getElementById('output');
+const inputField = document.getElementById('input');
 
-.terminal {
-    width: 80%;
-    max-width: 600px;
-    margin: 50px auto;
-    background-color: black;
-    border: 1px solid white;
-    padding: 10px;
-    box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
-}
-
-#input {
-    width: 100%;
-    border: none;
-    background: transparent;
-    color: white;
-    outline: none;
-}
+inputField.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        const command = inputField.value;
+        inputField.value = '';
+        terminal.innerHTML += `<div>$ ${command}</div>`;
+        
+        // Simulate a response (replace this with actual server-side processing)
+        setTimeout(() => {
+            const response = 'This is a simulated response to the command: ' + command;
+            terminal.innerHTML += `<div>${response}</div>`;
+        }, 500);
+    }
+});
